7cbcaa4fa0cf8e0f1bdd01bd097392abb2334bb5
Hannes | HiViCon <67242099+hannes813@users.noreply.github.com>
2026-05-25 22:09:20 +0200

    Update docker-run.sh

--- examples/docker-run.sh
+++ examples/docker-run.sh
@@ -3,11 +3,11 @@ set -eu
 
 docker run --rm -it --network host \
   -v /volume2/docker/zigbee2mqtt:/work \
-  -e MIELE_ZNP_HOST=192.168.178.101 \
+  -e MIELE_ZNP_HOST=192.168.xxx.xxx \
   -e MIELE_ZNP_PORT=6638 \
   -e MIELE_TARGET_NWK=0x537D \
-  -e MQTT_HOST=192.168.178.56 \
-  -e MQTT_USER=mqtt \
+  -e MQTT_HOST=192.168.xxx.xx \
+  -e MQTT_USER='your User' \
   -e MQTT_PASS='your-password' \
   -e MQTT_BASE=miele_xkm3000z \
   python:3.12-alpine \
